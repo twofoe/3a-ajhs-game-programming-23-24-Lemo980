d3bb945bd60fe3412d3538d8600618b568db08d7
EliotB <s11806270@students.duvalschools.org>
2023-09-29 11:49:19 -0400

    v0.1b
    Printing list contents

--- gameProgramming/computer_science_exercises/03_Collections/collections.py
+++ gameProgramming/computer_science_exercises/03_Collections/collections.py
@@ -10,3 +10,9 @@ breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
 #index -1 is the last item in the list
 testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1,74, 1.99, 0.99, 4.25]
+
+# printing contents of a list
+print (breakfastFoods)
+print (testScores)
+print (classGPA)
+
