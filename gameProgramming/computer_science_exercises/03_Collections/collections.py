bad97b4ab213a9283ad0afe9223196e16ad0bf58
EliotB <s11806270@students.duvalschools.org>
2023-09-29 11:57:34 -0400

    v0.1d
    WYOC -- access 3rd element
    comment out print statements

--- gameProgramming/computer_science_exercises/03_Collections/collections.py
+++ gameProgramming/computer_science_exercises/03_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Eliot Blanton, v0.1b
+# Collections Examples, Eliot Blanton, v0.1d
 
 #LIST -- Ordered Changeable, Allows duplicate values
 
@@ -9,24 +9,29 @@ breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
 #The element bacon is at index 0
 #index -1 is the last item in the list
 testScores = [95, 100, 25, 15, 27, 35]
-classGPA = [3.14, 2.25, 1,74, 1.99, 0.99, 4.25]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 
 # printing contents of a list
-print (breakfastFoods)
-print (testScores)
-print (classGPA)
+#print (breakfastFoods)
+#print (testScores)
+#print (classGPA)
 
 #Accessing specific list elements --  FIRST ELEMENT IS INDEX 0
-print(breakfastFoods[0])
-print(testScores[0])
-print(classGPA[0])
+#print(breakfastFoods[0])
+#print(testScores[0])
+#print(classGPA[0])
 
 #Accessing last element in list
 
-print(breakfastFoods[-1])
-print(testScores[-1])
-print(classGPA[-1])
+#print(breakfastFoods[-1])
+#print(testScores[-1])
+#print(classGPA[-1])
 
+#Pause -- WYOC -- Access 3rd element in each list
+
+#print(breakfastFoods[2])
+#print(testScores[2])
+#print(classGPA[2])
 
 
 
