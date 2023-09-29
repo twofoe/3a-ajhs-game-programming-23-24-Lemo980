b954cdebf1ca652185f0227b85fb1e9b38780150
EliotB <s11806270@students.duvalschools.org>
2023-09-29 11:46:52 -0400

    v0.1a
    Declared and defined 3 lists

--- gameProgramming/computer_science_exercises/03_Collections/collections.py
+++ gameProgramming/computer_science_exercises/03_Collections/collections.py
@@ -1,1 +1,12 @@
-# Collections Examples, Eliot Blanton, v0.0
+# Collections Examples, Eliot Blanton, v0.1a
+
+#LIST -- Ordered Changeable, Allows duplicate values
+
+breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
+
+#Each item in the list is known as an element
+#The postion in the list for each is known as the index
+#The element bacon is at index 0
+#index -1 is the last item in the list
+testScores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.25, 1,74, 1.99, 0.99, 4.25]
