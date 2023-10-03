6893a2949aa00d9d93f6c7e5d01cf2eae8f12360
EliotB <s11806270@students.duvalschools.org>
2023-10-03 13:19:39 -0400

    Create collections.py

---
+++ gameProgramming/gaming_exercises/02_collections/collections.py
@@ -0,0 +1,20 @@
+#Adding Items
+playerInventory = []
+while len(playerInventory) < 10:
+    item = input("Please choose an item to add to your inventory.\n")
+    playerInventory.append(item)
+playerInventory.sort()
+# . whatever is known as a method. It means "Do this to that".
+print(playerInventory)
+
+#Removing items
+while len(playerInventory) > 5:
+    item = input("Please choose an item to remove from your inventory.\n")
+    playerInventory.remove(item)
+playerInventory.sort()
+print(playerInventory)
+
+
+
+
+
