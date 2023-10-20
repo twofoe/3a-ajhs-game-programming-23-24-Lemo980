e5fa4baecdac3670caa8ca8e79f949d119ef88de
EliotB <s11806270@students.duvalschools.org>
2023-10-20 13:15:26 -0400

    Update functions.py
    code still not running correctly

--- gameProgramming/03b_functions/functions.py
+++ gameProgramming/03b_functions/functions.py
@@ -32,39 +32,46 @@ import random
 # example = functionWithoutReturn(5,10)
 # print(example)
 def rollDice(numRoll, sizeRoll):
-     count = 0
-     sum = 0
-     while count < numRoll: 
-         roll = random.randint (1,sizeRoll)
-         sum += roll
-         print(f"Roll # {count}: {roll}\n")
-         count += 1
-     print(sum)
-     return sum
+    count = 0
+    sum = 0
+    while count < numRoll: 
+        roll = random.randint (1, sizeRoll)
+        print(f"Roll # {count}: {roll}\n")
+        sum += roll
+        count += 1
+    #print(sum)
+    return sum
 
  #print("D4 rolls")
  #d4Sum = rollDice(10, 4)
  #print("D20 rolls")
  #d20Sum = rollDice(2, 20)
 
+#rollDice(18, 4)
+#rollDice(2, 20)
+
 def genStat(): #Roll 4 D6, drop the lowest
     rolls = []
     count = 0
+
     while count < 4:
         rolls.append(rollDice (1, 6))
         count += 1
-    
     rolls.sort()
     rolls.pop(0)
-    print (sum(rolls))
     return (sum(rolls))
 
 count = 0
-while count < 10000:
-    
-
-
-strength = genStat()
+while count < 1000000:
+     test = genStat()
+     if test > 18:
+         print("You have exceeded the maximum roll. \n")
+     if test < 3:
+         print("You have not met the minimum roll. \n")
+     count += 1
+              
+print("If no error messages, all stats are within range. \n")
+strength =genStat()
 print(f"Your strength: {strength}\n")
 
 
