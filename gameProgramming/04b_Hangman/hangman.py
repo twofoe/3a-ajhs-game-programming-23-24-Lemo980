1371af97ea272cd91c282b78aa37e17652a15721
EliotB <s11806270@students.duvalschools.org>
2023-11-01 11:55:35 -0400

    Update hangman.py

--- gameProgramming/04b_Hangman/hangman.py
+++ gameProgramming/04b_Hangman/hangman.py
@@ -1,5 +1,62 @@
-wordList = 'dog cow hat bird extraterrestrial undeniable disasterous extraterestrial fortitude alligator crocodile runner wheat foggy vision king loops showered typed untouchable invincible frog pool marshmallow buffet flying envious skeleton cranium'.split()
+wordList = 'dog cow hat bird extraterrestrial undeniable disasterous explosive fortitude alligator crocodile runner wheat foggy vision king loops showered typed untouchable invincible frog pool marshmallow buffet flying envious skeleton cranium'.split()
 #.split() will split a string into seperate elements, by default set to space
-print(wordList)
 
-print(wordList[0])
+#VARIABLE_NAMES that are ALL CAPS are constants and are not meant to change.
+HANGMAN_BOARD = ['''
+  x-----x
+        |
+        |
+        |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+        |
+        |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+  |     |
+        |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+  |     |
+        |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+  |\    |
+        |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+ /|\    |
+        |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+ /|\    |
+   \    |
+        |
+        |
+   0=========0 ''' , '''
+  x-----x
+  O     |
+ /|\    |
+ / \    |
+        |
+        |
+   0=========0''']
+
