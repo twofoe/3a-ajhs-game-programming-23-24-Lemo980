463ef3fea10903e1fa5918c50f5e8ff1952403ee
EliotB <s11806270@students.duvalschools.org>
2023-11-01 11:40:13 -0400

    Update hangman.py

--- gameProgramming/04b_Hangman/hangman.py
+++ gameProgramming/04b_Hangman/hangman.py
@@ -1,3 +1,5 @@
-wordList = [
-    "dog, cow, hat, bird, extraterrestrial, undeniable, disasterous"
-]
+wordList = 'dog cow hat bird extraterrestrial undeniable disasterous extraterestrial fortitude alligator crocodile runner wheat foggy vision king loops showered typed untouchable invincible frog pool marshmallow buffet flying envious skeleton cranium'.split()
+#.split() will split a string into seperate elements, by default set to space
+print(wordList)
+
+print(wordList[0])
