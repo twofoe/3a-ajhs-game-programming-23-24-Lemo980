c67dca542ffef244b1d1c42d9a8f5c6d3e0f9fc0
EliotB <s11806270@students.duvalschools.org>
2023-11-01 12:58:17 -0400

    Update hangman.py

--- gameProgramming/04b_Hangman/hangman.py
+++ gameProgramming/04b_Hangman/hangman.py
@@ -1,5 +1,5 @@
 import random
-wordList = 'dog cow hat bird extraterrestrial undeniable disasterous explosive fortitude alligator crocodile runner wheat foggy vision king loops showered typed untouchable invincible frog pool marshmallow buffet flying envious skeleton cranium'.split()
+wordList = 'dog cow hat bird extraterrestrial undeniable disasterous explosive fortitude alligator crocodile runner wheat foggy vision king loops showered typed untouchable invincible frog pool marshmallow buffet flying envious skeleton cranium gallows'.split()
 #.split() will split a string into seperate elements, by default set to space
 
 #VARIABLE_NAMES that are ALL CAPS are constants and are not meant to change.
@@ -54,15 +54,16 @@ O     |
        |
   0=========0''']
 
-# i = 0
-# while i < len(HANGMAN_BOARD):
-#     print(HANGMAN_BOARD[i])
-#     i += 1
 
 def getRandomWord(wordList):
-    pass
+    wordIndex = random.randint(0 , len(wordList) - 1)
+    #len(listName) - 1 is most common way to prevent index out of range errors.
+    return wordList[wordIndex]
 
-
+# i = 0
+# while i < 500000:
+#     getRandomWord(wordList)
+#     i += 1
 
 
 
