fd071374b8130db860a9a7e054d6bf8ca95c119b
EliotB <s11806270@students.duvalschools.org>
2023-11-14 13:06:27 -0500

    Update exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,8 +1,22 @@
 # Example Game Functions, Eliot Blanton, v0.0
 import random
 
-def func1():
-    pass
+#Variable Declarations
+weaponList = ["Wooden club", "Stone hammer", "Bone spear", "Magic staff of flame", "Steel greatsword", "Silver dagger", "Magical ice mace" ] #list of items in inventory
+hitPoints = 100 #how much total health a player has
+enemyList = ["Skeleton warrior", "Giant hog", "Dark mage", "Venomous serpent", "Goblin warrior", "Giant warrior"]
+equippedWeapon = getWeapon(weaponList) 
+
+
+
+
+
+#Function Definitions
+def getWeapon(weaponList):
+    equippedWeapon = weaponList[randint(0, 6)]
+    print("You have obtained a ")
+    return equippedWeapon
+    
 
 def func2():
     pass
