e34cc95b0cdbf6e73d5d5105ab6a57bd0445e094
EliotB <s11806270@students.duvalschools.org>
2023-11-14 13:21:30 -0500

    Update exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -4,22 +4,84 @@ import random
 #Variable Declarations
 weaponList = ["Wooden club", "Stone hammer", "Bone spear", "Magic staff of flame", "Steel greatsword", "Silver dagger", "Magical ice mace" ] #list of items in inventory
 hitPoints = 100 #how much total health a player has
-enemyList = ["Skeleton warrior", "Giant hog", "Dark mage", "Venomous serpent", "Goblin warrior", "Giant warrior"]
-equippedWeapon = getWeapon(weaponList) 
+enemyList = ["Skeleton warrior", "Giant hog", "Dark mage", "Venomous serpent", "Goblin warrior", "Giant warrior", "Tree monster", "Werewolf"]
+equippedWeapon = weaponList[random.randint(0, 6)] 
 
 
 
 
 
 #Function Definitions
-def getWeapon(weaponList):
-    equippedWeapon = weaponList[randint(0, 6)]
-    print("You have obtained a ")
-    return equippedWeapon
-    
 
-def func2():
+def enemyStats(enemyList, ):
     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def doDamage(equippedWeapon, enemyList):
+    enemy = enemyList[random.randint(0, 7)]
+    if equippedWeapon == "Wooden club":
+        if enemy == 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def func3():
     pass
