6d03b52c5802350719d51f244c998177be7d6728
EliotB <s11806270@students.duvalschools.org>
2023-11-14 11:43:06 -0500

    Update exampleGameFunctions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,76 @@
 # Example Game Functions, Eliot Blanton, v0.0
+import random
+
+def func1():
+    pass
+
+def func2():
+    pass
+
+def func3():
+    pass
+
+def func3():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
